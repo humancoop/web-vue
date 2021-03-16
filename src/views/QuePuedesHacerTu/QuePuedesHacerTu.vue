ddd8fe50289af060513a54858b3a65c3bb0b5d6c
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-03-16 18:29:48 +0100

    Migrate missing template

--- src/views/QuePuedesHacerTu/QuePuedesHacerTu.vue
+++ src/views/QuePuedesHacerTu/QuePuedesHacerTu.vue
@@ -1,50 +1,22 @@
-<template>
-  <h1 class="text-6xl text-center">¿Qué puedes hacer tú?</h1>
-	<div class="flex mb-4">
-		<div class="w-1/3 p-4">
-			<a href="/donacion">
-			<div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                <thumbor
-                  src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/dona.jpg"
-                  class="w-full"
-                  width="266"
-                  height=""
-                />
-				<div class="px-6 py-4">
-					<div class="font-bold text-xl mb-2">Haz una donación</div>
-				</div>
-			</div>
-			</a>
-		</div>
-		<div class="w-1/3 p-4">
-			<a href="/socio">
-			<div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                <thumbor
-                  src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/coche.jpg"
-                  class="w-full"
-                  width="266"
-                  height=""
-                />
-				<div class="px-6 py-4">
-					<div class="font-bold text-xl mb-2">Hazte socio</div>
-				</div>
-			</div>
-			</a>
-		</div>
-		<div class="w-1/3 p-4">
-			<a href="/voluntario">
-			<div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                <thumbor
-                  src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/manos.jpg"
-                  class="w-full"
-                  width="266"
-                  height=""
-                />
-				<div class="px-6 py-4">
-					<div class="font-bold text-xl mb-2">Hazte voluntario</div>
-				</div>
-			</div>
-			</a>
-		</div>
-	</div>
+<template lang="pug">
+section-title(text="¿Qué puedes hacer tú?")
+.flex.mb-4
+  div(class="w-1/3 p-4")
+    router-link(:to="{name:'donacion'}")
+      .max-w-sm.rounded.overflow-hidden.shadow-lg.bg-white
+        thumbor.w-full(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/dona.jpg" width="266" height="")
+        .px-6.py-4
+          .font-bold.text-xl.mb-2 Haz una donación
+  div(class="w-1/3 p-4")
+    router-link(:to="{name:'socio'}")
+      .max-w-sm.rounded.overflow-hidden.shadow-lg.bg-white
+        thumbor.w-full(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/coche.jpg" width="266" height="")
+        .px-6.py-4
+          .font-bold.text-xl.mb-2 Hazte socio
+  div(class="w-1/3 p-4")
+    router-link(:to="{name:'voluntario'}")
+      .max-w-sm.rounded.overflow-hidden.shadow-lg.bg-white
+        thumbor.w-full(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/manos.jpg" width="266" height="")
+        .px-6.py-4
+          .font-bold.text-xl.mb-2 Hazte voluntario
 </template>
