5902745549624dd31181c91af4e334fc7becb007
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-03-19 20:00:38 +0100

    Add more text content

--- src/views/TerritoriosLiberadosSaharauis/Farmacia.vue
+++ src/views/TerritoriosLiberadosSaharauis/Farmacia.vue
@@ -7,44 +7,44 @@ section-title(text="Farmacia")
     height=""
   )
 
-.text-justify.py-4.italic.text-lg.italic
-| ¿Sabes por qué fracasan la mayoría de los tratamientos en los pacientes crónicos de Territorios Liberados?
+.text-justify.py-4.italic.text-lg
+  | ¿Sabes por qué fracasan la mayoría de los tratamientos en los pacientes crónicos de Territorios Liberados?
 .text-justify.py-4.font-bold
-b No existe adherencia al tratamiento.
+  | No existe adherencia al tratamiento.
 .text-justify.py-4
-| A diario, lidian con la
-b  incertidumbre de si conseguirán la medicación para tratar sus problemas de salud.
-.text-justify.py-4
-| No saben si el próximo mes van a disponer de inhaladores para paliar los síntomas de sus problemas respiratorios.
-.text-justify.py-4
-| Ignoran si lograrán mantener la glucosa a raya. O si se les descompensará hasta el punto que no puedan moverse de su haima.
-.text-justify.py-4
-| O si los niños seguirán teniendo alteraciones gastrointestinales por falta de antiparasitarios, antibióticos con los que tratar las infecciones o productos de aseo mínimo que disminuyan los problemas en la piel.
+  | A diario, lidian con la
+  b  incertidumbre de si conseguirán la medicación para tratar sus problemas de salud.
+  br
+  | No saben si el próximo mes van a disponer de inhaladores para paliar los síntomas de sus problemas respiratorios.
+  br
+  | Ignoran si lograrán mantener la glucosa a raya. O si se les descompensará hasta el punto que no puedan moverse de su haima.
+  br
+  | O si los niños seguirán teniendo alteraciones gastrointestinales por falta de antiparasitarios, antibióticos con los que tratar las infecciones o productos de aseo mínimo que disminuyan los problemas en la piel.
 .text-justify.py-4.italic.text-lg.italic
-| ¿Y cuál es nuestro papel al respecto?
+  | ¿Y cuál es nuestro papel al respecto?
 .text-justify.py-4
-| Desde España, hacemos una previsión de medicación por paciente en torno a 4-6 meses - en función de sus patologías crónicas más frecuentes. Y la llevamos con nosotros.
-.text-justify.py-4
-| Una vez en terreno, y tras una exploración previa del paciente por un especialista, se le proporcionan los fármacos que necesita para resolver esa patología.
-.text-justify.py-4
-| Nuestra farmacia
-b  no es solo un dispensador de medicamentos... 
-.text-justify.py-4
-| Vamos más allá.
-.text-justify.py-4
-| Tratamos de
-b  ser una herramienta que les aporte información sobre su enfermedad y cómo tratarla.
-.text-justify.py-4
-| Qué deben de tomar, que comprendan cómo deben hacerlo y cómo no, con qué otra medicación pueden tener efectos adversos o que una medicina no es algo a lo que deban atarse por siempre jamás.
-.text-justify.py-4
-| Les
-b  ayudamos a conocerse
-|  y a lograr otros puntos de vista sobre
-b  cómo cuidarse con pequeños gestos que mejoren su estilo de vida.
+  | Desde España, hacemos una previsión de medicación por paciente en torno a 4-6 meses - en función de sus patologías crónicas más frecuentes. Y la llevamos con nosotros.
+  br
+  | Una vez en terreno, y tras una exploración previa del paciente por un especialista, se le proporcionan los fármacos que necesita para resolver esa patología.
+  br
+  | Nuestra farmacia
+  b  no es solo un dispensador de medicamentos... 
+  br
+  | Vamos más allá.
+  br
+  | Tratamos de
+  b  ser una herramienta que les aporte información sobre su enfermedad y cómo tratarla.
+  br
+  | Qué deben de tomar, que comprendan cómo deben hacerlo y cómo no, con qué otra medicación pueden tener efectos adversos o que una medicina no es algo a lo que deban atarse por siempre jamás.
+  br
+  | Les
+  b  ayudamos a conocerse
+  |  y a lograr otros puntos de vista sobre
+  b  cómo cuidarse con pequeños gestos que mejoren su estilo de vida.
 .text-justify.py-4.italic.text-lg.italic
-| Queremos que su salud esté en sus manos.
+  | Queremos que su salud esté en sus manos.
 .text-justify.py-4
-| ¿Te apuntas a este reto?
+  | ¿Te apuntas a este reto?
 
 squares
 </template>
--- src/views/TerritoriosLiberadosSaharauis/MedicinaGeneral.vue
+++ src/views/TerritoriosLiberadosSaharauis/MedicinaGeneral.vue
@@ -6,7 +6,20 @@ section-title(text="Medicina")
     width="896"
     height=""
   )
-| Falta texto
+.text-justify.py-4
+  | El área médica de HumanCoop tiene como objetivo general contribuir a la mejora del estado de salud de aquellas poblaciones más desfavorecidas, entendiendo la salud no solo como la ausencia de enfermedad, sino como el completo bienestar físico, psíquico y social.
+.text-justify.py-4
+  | Para ello sustenta su acción en tres aspectos fundamentales:
+ul 
+  li.mb-4 - Promoción de la salud y prevención de la enfermedad: a través de la creación de programas de salud comunitaria basados en estudios epidemiológicos de la zona y reuniones con los diferentes actores sociales, para diagnosticar y actuar sobre las causas reales de los problemas.
+  li.mb-4 - Fortalecimiento del sistema de salud local: mediante jornadas formativas de carácter clínico y de gestión, con el objetivo de establecer un sistema de atención primaria eficiente y de calidad a través de los profesionales en salud y el apoyo de la sinstituciones. Además apoyamos la existencia de un servicio de farmacia actualizado y competente, así como la disponibilidad de los recursos materiales necesarios.
+  li.mb-4 - Realización de campañas de asistencia sanitaria con voluntarios, médicas y quirúrgicas: de forma periódica asistimos a terreno para acompañar a los actores en salud en su consulta habitual, además de ampliar la atención médica a otras áreas inexistentes o de difícil acceso y realizar un registro de pacientes con patología quirúrgica para eventuales comisiones específicas en este ámbito.
+
+.text-justify.py-4
+  | Y todo ello ciñéndonos a las máximas del respeto, la sostenibilidad y la horizontalidad. Porque nos consideramos un grupo de personas siempre dispuestas al servicio de las necesidades de la comunidad, sabiendo que los verdaderos protagonistas del proyecto son los trabajdores locales y los propios beneficiarios.
+
+.text-justify.py-4
+  | Este área la formamos un grupo de profesionales sanitarios de muy diversas especialidades y ámbitos, siempre abiertos a nuevas ideas e incorporaciones que ayuden a conseguir los objetivos comunes. Tratamos de tejer redes con otras organizaciones y profesionales que colaboren en las líneas descritas, por lo que nos consideramos un grupo dinámico y en continuo crecimiento.
 squares
 </template>
 <script lang="ts">
