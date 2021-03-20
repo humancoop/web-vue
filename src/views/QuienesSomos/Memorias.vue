654f9c7dbba26f12cecda272d5be51ff8a9d8ba0
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-03-20 10:53:37 +0100

    Add new pdfs

--- src/views/QuienesSomos/Memorias.vue
+++ src/views/QuienesSomos/Memorias.vue
@@ -6,13 +6,23 @@ thumbor.object-cover.w-full(
   width="896"
   height=""
 )
-.flex.mb-4
-  div(class="w-5/6")
-    p.text-justify.py-4
-      a(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2019.pdf")
-        thumbor(
-          src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2019.png"
-          width="200"
-          height=""
-        )
+.flex.my-4
+  a.mx-4(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2019.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2019.png"
+      width="200"
+      height=""
+    )
+  a.mx-4(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2018.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2018.png"
+      width="200"
+      height=""
+    )
+  a.mx-4(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2017.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2017.png"
+      width="200"
+      height=""
+    )
 </template>
