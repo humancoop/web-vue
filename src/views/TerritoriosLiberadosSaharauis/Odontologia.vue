0eecaf97cb4d78d2c0bb51cfd96455517baa30e9
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-05-09 12:56:32 +0200

    Update text

--- src/views/TerritoriosLiberadosSaharauis/Odontologia.vue
+++ src/views/TerritoriosLiberadosSaharauis/Odontologia.vue
@@ -6,48 +6,34 @@ section-title(text="Odontología")
     width="896"
     height=""
   )
-h2.mt-4.text-4xl Plan de escuelas de salud bucodental
+.text-justify.py-4.font-bold
+  | Asistencia odontológica:
 .text-justify.py-4
-  | Con el Plan de Escuelas, queremos garantizar que todo el alumnado realice en la escuela:
-  ul
-    li - Un cepillado diario.
-    li - Una aplicación de flúor (la temporalidad del flúor depende del tipo aplicado).
+  | En los Territorios Liberados del Sáhara Occidental  el acceso a la atención bucodental es muy limitado por la falta de personal local cualificado. El Ministerio de Salud Pública de la RASD (República Árabe Saharaui Democrática) organiza en esta zona comisiones especializadas que se desplazan desde los Campamentos de Refugiados en Tindouf, una o dos veces al año, para ofrecer  asistencia en el campo de la odontología y otras áreas, pero obviamente la demanda de la población es mayor y estas comisiones médicas son claramente insuficientes
 .text-justify.py-4
-  | Queremos ayudar a crear generaciones de niños y niñas saharauis con hábitos
-  | saludables en materia de higiene bucodental y nutrición, que contribuyan a mejorar
-  | sus expectativas de bienestar físico, psíquico y social en el futuro.
+  | En HumanCoop Sahara hacemos una función de apoyo y complemento al Ministerio, con la organización de comisiones médicas compuestas por nuestros odontólogos voluntarios, para dar asistencia dental (obturaciones, exodoncias, tratamientos periodontales...) a la población saharaui de la zona Liberada. Desde 2014 hasta 2020 se han organizado 30 comisiones, a través de nuestra ONG hermana DentalCoop Sahara, que han atendido unas 10.000 consultas.
+.text-justify.py-4.font-bold
+  | Plan de Salud Bucodental Escolar:
 .text-justify.py-4
-  | El programa favorecerá, por un lado, la creación de un hábito de vida saludable
-  | para el futuro de los niños y niñas saharauis y, por otro, fomentará, a través de la
-  | educación en salud, la importancia de las revisiones anuales odontológicas y el
-  | mantenimiento preventivo para evitar enfermedades bucodentales.
-h2.text-4xl Plan de prótesis dental
+  | Como complemento a esta asistencia, uno de nuestros principales objetivos es reforzar la prevención, a través del Plan de Salud Bucodental Escolar, cuyo fin es aportar información a los profesores y alumnos sobre las técnicas de higiene dental y el cuidado de la dieta, así como la introducción del cepillado diario en la propia escuela. Queremos ayudar a crear generaciones de niños y niñas saharauis con hábitos saludables en materia de higiene bucodental y nutrición, que contribuyan a mejorar sus expectativas de bienestar físico, psíquico y social en el futuro.
 .text-justify.py-4
-  | Una gran proporción de la población saharaui nómada de esta zona sufre pérdida
-  | parcial o total de piezas dentarias, debido al consumo de té muy azucarado,
-  | escasa o nula atención odontológica, y ausencia casi total de consciencia sobre
-  | higiene bucodental.
+  | El programa fomentará además, a través de la educación en salud, el conocimiento de la importancia de las revisiones anuales odontológicas y el mantenimiento preventivo para evitar enfermedades bucodentales.
+.text-justify.py-4.font-bold
+  | Plan de Prótesis Dental:
 .text-justify.py-4
-  | En muchas ocasiones, en las consultas de las Comisiones Dentales se acaban por
-  | extraer varias piezas, sin posibilidad, a día de hoy, de su sustitución por prótesis
-  | dentales. Por ello, muchos pacientes son reacios a acudir a las revisiones, ya que
-  | saben que no se les van a rehabilitar sus piezas perdidas. Con el Servicio de
-  | Prótesis pueden cambiar la visión generalizada y acudir más al dentista, ya que se
-  | podrán reponer dichas piezas. También se dará solución a los problemas
-  | mandibulares, digestivos, de fonación, encías y estéticos.
+  | A través del Plan de Prótesis Dental tratamos de evitar o mejorar los problemas derivados de la falta de piezas dentales. Aportamos el material y los técnicos especializados en esta materia para la elaboración de prótesis removibles.
 .text-justify.py-4
-  | Actividades:
-  ul 
-    li - Re-instalación y mantenimiento del laboratorio de Rabouni en los Campamentos de Refugiados de Tindouf.
-    li - Instalación de laboratorio de prótesis en el Hospital de Tifariti (T. Liberados).
-    li - Comisiones periódicas.
-    li - Formación de profesionales locales para su empoderamiento.
+  | Una gran proporción de la población saharaui nómada de esta zona sufre pérdida parcial o total de piezas dentarias, debido al consumo de té muy azucarado, escasa o nula atención odontológica, y ausencia casi total de consciencia sobre higiene bucodental.
 .text-justify.py-4
-  ul 
-    li - Te gusta la cooperación internacional? ¡Hazte voluntario, te necesitamos!
-    li - ¿Te sumas a cambiar el Mundo?
-    li - ¿Eres Protésico?
-squares
+  | En muchas ocasiones, en las consultas de las comisiones dentales se acaban por extraer varias piezas, sin posibilidad, a día de hoy, de su sustitución por prótesis dentales. Por ello, muchos pacientes son reacios a acudir a las revisiones, ya que saben que no se les van a rehabilitar sus piezas perdidas. Con la posibilidad de elaborar prótesis dentales pueden cambiar la visión generalizada y acudir más al dentista. También se dará solución a los problemas mandibulares, digestivos, de fonación, de encías y estéticos.
+.text-justify.py-4
+  | Este Plan de Prótesis incluye la re-instalación y mantenimiento del laboratorio de Rabouni en los Campamentos de Refugiados de Tindouf, la instalación de un laboratorio de prótesis en el Hospital de Tifariti (T. Liberados), comisiones periódicas de protésicos voluntarios, y la formación de profesionales locales para su empoderamiento.
+ul.font-bold
+  li - ¿Eres dentista? ¿Auxiliar? ¿Higienista? ¿Protésico?
+  li - ¿Te gusta la cooperación internacional? ¡Hazte voluntario, te necesitamos!
+  li  - ¿Te sumas a cambiar el Mundo?
+
+quares
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
