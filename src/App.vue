a3f597173a5b7a912166076c0a4fb7ac099cb4e0
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-12-26 20:00:03 +0100

    Reorganize Navbar components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template lang="pug">
-header
-  nav.flex.items-center.justify-between.flex-wrap.bg-gray-900.p-6
-    logo
-    mobile-menu
-    div(class="w-full hidden lg:block flex-grow lg:flex lg:items-center lg:w-auto")
-      social-links
-      link-buttons
-  navigation-links
+navbar
 main.mb-auto.mx-auto.text-gray-900.max-w-4xl
   router-view
 footer.text-center.w-full.mt-16.bg-gray-900.p-4
@@ -15,20 +8,12 @@ footer.text-center.w-full.mt-16.bg-gray-900.p-4
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import  NavigationLinks from './components/NavigationLinks.vue'
-import  Logo from './components/Logo.vue'
-import  MobileMenu from './components/MobileMenu.vue'
-import  SocialLinks from './components/SocialLinks.vue'
-import  LinkButtons from './components/LinkButtons.vue'
+import  Navbar from './components/Navbar/Navbar.vue'
 import  FooterContent from './components/FooterContent.vue'
 export default defineComponent({
   components: {
-    NavigationLinks,
-    Logo,
-    MobileMenu,
-    SocialLinks,
-    LinkButtons,
     FooterContent,
+    Navbar,
   }
 })
 </script>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+header
+  nav.flex.items-center.justify-between.flex-wrap.bg-gray-900.p-6
+    logo
+    mobile-menu
+    div(class="w-full hidden lg:block flex-grow lg:flex lg:items-center lg:w-auto")
+      social-links
+      link-buttons
+  navigation-links
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import  NavigationLinks from './NavigationLinks.vue'
+import  Logo from './Logo.vue'
+import  MobileMenu from './MobileMenu.vue'
+import  SocialLinks from './SocialLinks.vue'
+import  LinkButtons from './LinkButtons.vue'
+export default defineComponent({
+  name: 'NavBar',
+  components: {
+    NavigationLinks,
+    Logo,
+    MobileMenu,
+    SocialLinks,
+    LinkButtons,
+  }
+})
+</script>
