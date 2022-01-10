f13f2bce8c08d5cefa7cf5e38f776b4deb260a44
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-01-10 15:26:07 +0100

    Fix SVG

--- src/components/Navbar/MobileMenu.vue
+++ src/components/Navbar/MobileMenu.vue
@@ -1,9 +1,19 @@
 <template lang="pug">
 div(class="block lg:hidden")
     button(class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white")
-    svg( class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg")
-        title Menu
-        path(d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z")
+      svg(
+        class="block h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      )
+        path(
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        )
 
 </template>
 
