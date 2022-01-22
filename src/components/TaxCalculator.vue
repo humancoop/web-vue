21b39dd6be78bd02bec9fed3b0bf51577ba46b88
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-01-22 14:01:34 +0100

    Responsive style fix

--- src/components/TaxCalculator.vue
+++ src/components/TaxCalculator.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-.flex.items-center
-  span(class="text-3xl") Te podrías desgravar&nbsp;
-  span(class="text-3xl text-teal-200") {{result}} €
-  span(class="text-3xl") &nbsp;al año
+p(class="text-3xl whitespace-nowrap")
+  | Te podrías desgravar&nbsp;
+  span(class="text-teal-200") {{result}} €
+  | &nbsp;al año
 p.mt-4.text-justify Hasta 150€ podrías desgravar el 80% de las donaciones. De los primeros 150 € deduces el 80%.
 p De la cantidad que supera esos primeros 150 € puedes deducirte el 40%, si has donado en los dos ejercicios anteriores una cantidad igual o superior al año anterior y el 35% en el resto de los casos.
 p Las deducciones difieren en País Vasco y Navarra.
