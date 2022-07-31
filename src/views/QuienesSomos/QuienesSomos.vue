a64813ebbbcdb76f92597e6407deeb56e1687984
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-07-31 11:30:10 +0200

    Fix menu for small displays

--- src/views/QuienesSomos/QuienesSomos.vue
+++ src/views/QuienesSomos/QuienesSomos.vue
@@ -1,8 +1,26 @@
 <template lang="pug">
 section-title(text="¿Quiénes somos?")
 cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/manos.jpg")
+div(class="lg:hidden")
+  div(class="flex justify-between")
+    a(href="/quienes-somos/voluntarios")
+      .max-w-sm.overflow-hidden.bg-gray-500
+        .px-1.py-1
+          .font-bold.mb-2 Voluntarios
+    a(href="/quienes-somos/junta")
+      .max-w-sm.overflow-hidden.bg-gray-500
+        .px-1.py-1
+          .font-bold.mb-2 Junta
+    a(href="/quienes-somos/estatutos")
+      .max-w-sm.overflow-hidden.bg-gray-500
+        .px-1.py-1
+          .font-bold.mb-2 Estatutos
+    router-link(:to="{'name': 'memorias'}")
+      .max-w-sm.overflow-hidden.bg-gray-500
+        .px-1.py-1
+          .font-bold.mb-2 Memorias
 .flex.mb-4
-  div(class="w-5/6 pr-6")
+  div(class="lg:w-5/6 lg:pr-6")
     .text-justify.py-4
       | HumanCoop es una Asociación de Voluntarios para la Cooperación y el Desarrollo,
       | que nace en 2019 para actuar en países y zonas del mundo en las que su
@@ -34,7 +52,8 @@ cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/manos.jpg")
       | conectamos con Personas, y siempre damos la máxima importancia al voluntario y
       | a su experiencia en este proyecto.
       | ¿TE SUMAS A CAMBIAR EL MUNDO?
-  div(class="w-1/6")
+
+  div(class="hidden lg:w-1/6 lg:block")
     a(href="/quienes-somos/voluntarios")
       .max-w-sm.overflow-hidden.bg-gray-500.my-2
         .px-6.py-4
@@ -51,4 +70,5 @@ cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/manos.jpg")
       .max-w-sm.overflow-hidden.bg-gray-500.my-2
         .px-6.py-4
           .font-bold.text-xl.mb-2 Memorias
+
 </template>
