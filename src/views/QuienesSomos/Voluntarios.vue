b6d4fe962d6b466b2d9d317b65982a29cdf5b944
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-08-27 15:04:27 +0200

    Hide Voluntarios

--- src/views/QuienesSomos/Voluntarios.vue
+++ src/views/QuienesSomos/Voluntarios.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
-section-title(text="Voluntarios")
-cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/portada_estatutos2.png")
-.flex.mt-8
-  video.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/acupuntor.png")
+section-title(text="En construcción...")
+//cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/portada_estatutos2.png")
+//.flex.mt-8
+  //video.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/acupuntor.png")
     source(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/acupuntor.mp4" type="video/mp4")
     | Sorry, your browser doesn't support embedded videos.
-  video.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/matrona.png")
+  //video.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/matrona.png")
     source(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/matrona.mp4" type="video/mp4")
     | Sorry, your browser doesn't support embedded videos.
-  video.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/odontologo.png")
+  //svideo.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/odontologo.png")
     source(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/odontologo.mp4" type="video/mp4")
     | Sorry, your browser doesn't support embedded videos.
 </template>
