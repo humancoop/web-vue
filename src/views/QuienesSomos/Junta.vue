779ba081e382017d7342e09bde34d4985681f6ae
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-08-27 14:05:42 +0200

    Add new sections

---
+++ src/views/QuienesSomos/Junta.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+section-title(text="Junta")
+cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/portada_estatutos2.png")
+.flex.flex-col
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/ignacio.jpg"
+    fullname="Ignacio"
+    title="Presidente"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/esther.jpg"
+    fullname="Esther"
+    title="Vicepresidenta"
+    other="Corresponsable del área de Laboratorio"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/eugenia.jpg"
+    fullname="Eugenia"
+    title="Secrentaria y Tesorera"
+    other="Responsable del área de Ginecología"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/gema2.jpg"
+    fullname="Gema"
+    title="Vocal de Comunicación"
+    other="Responsable del área de Farmacia"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/cristina.jpg"
+    fullname="Cristina"
+    title="Vocal de Financiación y Proyectos"
+    other="Responsable del área de Oftalmología"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/chiqui.jpg"
+    fullname="Chiqui"
+    title="Responsable del área de Prótesis Dentales"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/edu.jpg"
+    fullname="Edu"
+    title="Corresponsable del área de Laboratorio"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/iris.jpg"
+    fullname="Iris"
+    title="Responsable del área de Pediatría"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/jero.jpg"
+    fullname="Jero"
+    title="Responsable del área de Medicina"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/javi.jpg"
+    fullname="Javi"
+    title="Responsable del área de Farmacia"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/oscar2.jpg"
+    fullname="Óscar"
+    title="Responsable del área de Odontología"
+  )
+  member(
+    src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/rut.jpeg"
+    fullname="Ruth"
+    title="Responsable del área de Veterinaria"
+  )
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import  Member from './components/Member.vue'
+export default defineComponent({
+  components: {
+    Member,
+  }
+})
+</script>
--- src/views/QuienesSomos/Memorias.vue
+++ src/views/QuienesSomos/Memorias.vue
@@ -2,31 +2,31 @@
 section-title(text="Memorias anuales")
 cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/portada_memorias.jpg")
 .flex.mt-8
-  a.mx-auto(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2021.pdf")
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2021.pdf")
     thumbor(
       src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2021.png"
       width="200"
       height=""
     )
-  a.mx-auto(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2020.pdf")
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2020.pdf")
     thumbor(
       src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2020.png"
       width="200"
       height=""
     )
-  a.mx-auto(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2019.pdf")
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2019.pdf")
     thumbor(
       src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2019_2.png"
       width="200"
       height=""
     )
-  a.mx-auto(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2018.pdf")
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2018.pdf")
     thumbor(
       src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2018.png"
       width="200"
       height=""
     )
-  a.mx-auto(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2017.pdf")
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/memoriaActividades2017.pdf")
     thumbor(
       src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_memoria_2017.png"
       width="200"
