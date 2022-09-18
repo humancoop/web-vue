3df496c05d1dd97c305be6603fd11162a75617c5
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-09-18 18:49:01 +0200

    New logo

--- src/views/QueHacemos/QueHacemos.vue
+++ src/views/QueHacemos/QueHacemos.vue
@@ -4,5 +4,5 @@ section-title(text="¿Qué hacemos?")
 .p-4
   router-link(:to="{name:'territorios-liberados-saharauis'}")
     .max-w-sm.rounded.shadow-lg.p-4.bg-white
-      thumbor.w-full(width="352" height="" src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/Human-coop-sahara.png")
+      thumbor.w-full(width="352" height="" src="https://videos-humancoop.s3.eu-west-1.amazonaws.com/HC-Sahara.png")
 </template>
