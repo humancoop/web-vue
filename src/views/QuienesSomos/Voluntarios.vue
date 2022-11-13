b2ee0f10a658170db4a39aa1a61635db087e03e9
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-11-13 13:02:35 +0100

    Fill in Voluntarios

--- src/views/QuienesSomos/Voluntarios.vue
+++ src/views/QuienesSomos/Voluntarios.vue
@@ -1,14 +1,39 @@
 <template lang="pug">
-section-title-long(text="En construcción...")
-//cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/portada_estatutos2.png")
-//.flex.mt-8
-  //video.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/acupuntor.png")
-    source(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/acupuntor.mp4" type="video/mp4")
+section-title(text="Voluntarios")
+cover-image(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/portada_memorias.jpg")
+.flex.mt-8
+  video(controls poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/javier.png")
+    source(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/javier.mp4" type="video/mp4")
     | Sorry, your browser doesn't support embedded videos.
-  //video.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/matrona.png")
-    source(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/matrona.mp4" type="video/mp4")
-    | Sorry, your browser doesn't support embedded videos.
-  //svideo.px-4(controls width="100%" poster="https://videos-humancoop.s3-eu-west-1.amazonaws.com/odontologo.png")
-    source(src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/odontologo.mp4" type="video/mp4")
-    | Sorry, your browser doesn't support embedded videos.
+.flex.mt-8
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/caroline.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_caroline.png"
+      width="200"
+      height=""
+    )
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/elena.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_elena.png"
+      width="200"
+      height=""
+    )
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/lucas.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_lucas.png"
+      width="200"
+      height=""
+    )
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/mariluz.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_mariluz.png"
+      width="200"
+      height=""
+    )
+  a.mx-auto.px-2(href="https://videos-humancoop.s3-eu-west-1.amazonaws.com/noelia.pdf")
+    thumbor(
+      src="https://videos-humancoop.s3-eu-west-1.amazonaws.com/preview_noelia.png"
+      width="200"
+      height=""
+    )
 </template>
